0eb70c6e989c55267ad324297421313f645f4966
rvanheest <richard.v.heest@gmail.com>
2020-08-23 00:36:40 +0200

    simplify Observable examples

--- src/components/rxjs/console/Console.tsx
+++ src/components/rxjs/console/Console.tsx
@@ -5,34 +5,34 @@ import "./Console.css"
 interface Demo<T = any> {
     description: string | JSX.Element
     text: string
-    stream$: () => Observable<T>
+    stream$: Observable<T>
 }
 
 const demos: Demo[] = [
     {
         description: <code>never()</code>,
         text: "never",
-        stream$: () => NEVER,
+        stream$: NEVER,
     },
     {
         description: <code>empty()</code>,
         text: "empty",
-        stream$: () => EMPTY,
+        stream$: EMPTY,
     },
     {
         description: <code>of(1, 2, 3)</code>,
         text: "of(1, 2, 3)",
-        stream$: () => of(1, 2, 3),
+        stream$: of(1, 2, 3),
     },
     {
         description: <code>from([1, 2, 3, 4, 5, 6])</code>,
         text: "from([1, 2, 3, 4, 5, 6])",
-        stream$: () => from([1, 2, 3, 4, 5, 6]),
+        stream$: from([1, 2, 3, 4, 5, 6]),
     },
     {
         description: <span><code>create</code> - 3x <em>next</em>, then <em>complete</em></span>,
         text: "create - 3x next, then complete",
-        stream$: () => new Observable(observer => {
+        stream$: new Observable(observer => {
             observer.next(1)
             observer.next(2)
             observer.next(3)
@@ -42,7 +42,7 @@ const demos: Demo[] = [
     {
         description: <span><code>create</code> - 3x <em>next</em>, then <em>error</em></span>,
         text: "create - 3x next, then error",
-        stream$: () => new Observable(observer => {
+        stream$: new Observable(observer => {
             observer.next(1)
             observer.next(2)
             observer.next(3)
@@ -53,12 +53,12 @@ const demos: Demo[] = [
     {
         description: <code>range(0, 10)</code>,
         text: "range(0, 10)",
-        stream$: () => range(0, 10),
+        stream$: range(0, 10),
     },
     {
         description: <code>interval(1000)</code>,
         text: "interval(1000)",
-        stream$: () => interval(1000),
+        stream$: interval(1000),
     },
 ]
 
@@ -69,7 +69,7 @@ const Console = () => {
 
     useEffect(() => {
         appendToOutput(`${activeDemo.text}:`, { newline: true })
-        const subscription = activeDemo.stream$()
+        const subscription = activeDemo.stream$
             .subscribe(
                 x => appendToOutput(`next: ${x}`, { indent: true }),
                 e => appendToOutput(`error: ${e.message}`, { indent: true }),
